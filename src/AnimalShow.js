f226985c11862f28a275c465dc50fd538f5e76e2
Wes Holkeboer <wes.holkeboer@gmail.com>
2023-02-24 17:25:12 -0500

    added images and built out most functionality

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,5 +1,33 @@
-function AnimalShow() {
-  return <div>cow</div>;
+import { useState } from "react";
+import bird from "./svg/bird.svg";
+import cat from "./svg/cat.svg";
+import cow from "./svg/cow.svg";
+import dog from "./svg/bird.svg";
+import gator from "./svg/gator.svg";
+import heart from "./svg/heart.svg";
+import horse from "./svg/horse.svg";
+
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+};
+
+function AnimalShow({ type }) {
+  const [clicks, setClicks] = useState(0);
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
+  return (
+    <div onClick={handleClick}>
+      <img alt={type} src={svgMap[type]} />
+      <img alt="heart" src={heart} style={{ width: 10 + 10 * clicks + "px" }} />
+    </div>
+  );
 }
 
 export default AnimalShow;
--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+const getRandomAnimal = () => {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
   const handleClick = () => {
-    console.log("button was clicked");
-    setCount(count + 1);
+    setAnimals([...animals, getRandomAnimal()]);
   };
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
 
   return (
     <div>
       <button onClick={handleClick}>add animal</button>
-      <div>Number of animals: {count}</div>
+      <div>{renderedAnimals}</div>
+      {/* {animals.map((animal) => {
+        return <AnimalShow type={animal} />;
+      })} */}
     </div>
   );
 }
